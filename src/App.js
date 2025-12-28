f37467f9898a7e9faf6c90ce3f14ec56d28daa12
Ruksana0303 <shaikruksana0303@gmail.com>
2025-12-28 20:17:36 +0530

    FLeet management react app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
        <Routes>
-        <Route path="/login" element={<Login />}/>
+        <Route path="/login" element={<Login />} />
 
         <Route 
           path="/admin"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,6 +35,14 @@ const Login=()=>{
                 onChange={(e)=> setEmail(e.target.value)}
             />
             <br />
+
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+            />
+            <br />
             <button onClick={handleLogin}>Login</button>
         </div>
     );
